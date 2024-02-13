1a6ed10c2691ee064d240335b1f554650e5ffb6c
TalyaBenAtar <159432533+TalyaBenAtar@users.noreply.github.com>
2024-02-13 12:21:08 +0200

    finished
    the main functions are finished (the ones that efrat wanted us to have + init+ print+ some additional functions

--- airport.c
+++ airport.c
@@ -1,34 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "../include/airport.h"
 
-char* getAirportName()
+
+int initAirport(Airport* pAirport)
 {
-	char* name;
-	name = (char*)malloc(MAX_LENGTH * sizeof(char));
+    printf("Enter airport code  - 3 UPPER CASE letters");
+    char code;
+    do {
+        scanf("%3s", &code);
+    } while (!isCodeValid(&code));
+
+    for (int i=0; i<IATA; i++)
+    {
+        pAirport->code[i] = code;
+    }
+
+    printf("Enter airport name\n");
+    scanf("%s", &pAirport->name);
+
+    printf("Enter airport country");
+    scanf("%s", &pAirport->country);
+
+    pAirport->countPlanes=0;
 
 }
-void getAttributes()
-{
-	
-}
 
-void printAirport()
+int isCodeValid(const char* code)
 {
+    if (strlen( code) != IATA) {
+        printf("Code should be 3 letters.\n");
+        return 0;
+    }
 
+    for (int i = 0; i < IATA; i++)
+    {
+        if (!isupper(code[i]))
+        {
+            printf("Need to be upper case letters.\n");
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 int isSameAirport(Airport* airportPtr1, Airport* airportPtr2)
 {
-    if (*(airportPtr1->code)==*(airportPtr2->code)) {
-        return 1;
-    }else return 0;
+    for (int i = 0; i < IATA; i++){
+        if (airportPtr1->code[i] == airportPtr2->code[i])
+            return 0;
+    }return 1;
 }
 
-int isAirportCode(Airport* airportPtr, char code)
+int isAirportCode(Airport* airportPtr, const char* code)
 {
-	if (*(airportPtr->code)== code) {
-        return 1;
-    }else return 0;
+    for (int i = 0; i < IATA; i++){
+        if (airportPtr->code[i] != code[i])
+            return 0;
+    }return 1;
 }
+
+void printAirport(Airport* pAirport)
+{
+    printf("Airport name:");
+    printWithUnderscores((const char *) pAirport->name);
+
+    printf("A                  Country: %s", pAirport->country);
+
+    printf("                 Code:%c%c%c", pAirport->code[0], pAirport->code[1], pAirport->code[2]);
+
+}
+    void printWithUnderscores(const char *input)
+    {
+        while (*input != '\0') {
+            printf("%c_", *input);
+            input++;
+        }
+        printf("\n");
+    }
