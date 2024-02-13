850cd056be122d586dc610c4bb2dafcb025cf06c
TalyaBenAtar <159432533+TalyaBenAtar@users.noreply.github.com>
2024-02-13 12:21:49 +0200

    finished

--- airport.h
+++ airport.h
@@ -7,15 +7,17 @@
 #define MAX_LENGTH 100
 
 typedef struct {
-	char name;
+	char name[MAX_LENGTH];
 	char country;
 	char code[IATA];
     int countPlanes;
 } Airport;
 
-void getAttributes();
+int isCodeValid(const char* code);
+int initAirport(Airport* pAirport);
 void printAirport();
 int isSameAirport(Airport* airportPtr1, Airport* airportPtr2); // check by IATA code
-int isAirportCode(Airport* airportPtr, char code);
+int isAirportCode(Airport* airportPtr, const char* code);
+void printWithUnderscores(const char *input);
 
 #endif
