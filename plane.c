8bb0f4a3b6517cad39928225cd3d14dbf3634718
TalyaBenAtar <159432533+TalyaBenAtar@users.noreply.github.com>
2024-02-13 17:29:57 +0200

    maybe finished- let me know what you think

--- plane.c
+++ plane.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "plane.h"
-#include "helper.h"
+#include "../include/plane.h"
 
-void initPlane()
+
+void initPlane(Plane* pPlane)
 {
-	char planeType[MAX_STRING];
-}
+    do {
+        printf("Enter plane serial number - between 1 to 9999\n");
+        scanf("%d", &(pPlane->serielNumber));
+    }while (pPlane->serielNumber<MIN_SERIEL_NUMBER || pPlane->serielNumber>MAX_SERIEL_NUMBER);
 
+    int planeTypeNum;
+
+    do {
+        printf("Please enter one of the following types\n"
+               "0 for COMMERCIAL\n"
+               "1 for Cargo\n"
+               "2 for Military\n");
+
+        scanf("%d", &planeTypeNum);
+
+    } while ((planeTypeNum)>PLANE_TYPE_MAX || (planeTypeNum)<PLANE_TYPE_MIN);
+
+    if (planeTypeNum==COMMERCIAL){
+        pPlane->planeType  ="COMMERCIAL";
+    } else if (planeTypeNum==CARGO){
+        pPlane->planeType="CARGO";
+    } else if (planeTypeNum==MILIARY) {
+        pPlane->planeType = "MILIARY";
+    }
+
+
+}
 void printPlane(Plane* pPlane)
 {
-	printf("Plane: serial number:%d, type %s", pPlane->serielNumber, pPlane->planeType);
+    printf("Plane: serial number:%d, type %s", pPlane->serielNumber, pPlane->planeType);
+}
+
+void freePlane(Plane* pPlane){
+    free(pPlane->planeType);
 }
