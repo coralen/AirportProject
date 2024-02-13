3887179b20c2c79ac2fc115fe713a92930952d83
TalyaBenAtar <159432533+TalyaBenAtar@users.noreply.github.com>
2024-02-13 17:31:30 +0200

    Add files via upload

--- plane.h
+++ plane.h
@@ -1,13 +1,23 @@
 #ifndef _PLANE_H
 #define _PLANE_H
 
+#include "utilities.h"
+
+#define PLANE_TYPE_MAX 2
+#define PLANE_TYPE_MIN 0
+
+typedef enum { COMMERCIAL, CARGO, MILIARY } PlaneType;
+
+#define MAX_SERIEL_NUMBER 9999
+#define MIN_SERIEL_NUMBER 1
+
 typedef struct
 {
-	char* planeType;
-	int serielNumber; // between 1 and 9999
+    char* planeType;
+	int serielNumber;
 } Plane;
 
 void getAttributes();
-void printPlane(const Plane* pPlane);
+void printPlane();
 
 #endif
