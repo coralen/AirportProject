c4349a590798eb57317cc869b02d36fbd077c1cc
TalyaBenAtar <159432533+TalyaBenAtar@users.noreply.github.com>
2024-02-17 13:40:31 +0200

    Update Airline.c

--- Airline.c
+++ Airline.c
@@ -157,7 +157,7 @@ void printPlanesArr(const Plane* planeArr, const int planeCount)
 void freeCompany(Airline* pAirline)
 {
 	for (int i = 0; i < pAirline->flightCount; i++)
-		freeFlight(pAirline->flightArr[I]);
+		freeFlight(pAirline->flightArr[i]);
 	free(pAirline->flightArr);
 	for (int j = 0; j < pAirline->planeCount; j++)
 		freePlane(&pAirline->planeArr[j]);
