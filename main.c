25f006b4a1c198df95d3d9ae9f9e563cc57ed5f4
Coral Engel <81955555+coralen@users.noreply.github.com>
2024-02-18 20:26:25 +0200

    MAIN

--- main.c
+++ main.c
@@ -1,24 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "Plane.h"
 #include "Airline.h"
+#include "Airport.h"
+#include "AirportManager.h"
+#include "Flight.h"
+#include "Plane.h"
+#include "Date.h"
+#include "Helper.h"
 
 int main()
 {
+	AirportManager manager;
+	initManager(&manager);
 	Airline company;
-	//This function gets the pointer to the struct airline
-	//The function ask the user for airline name and set other
-	// arrays to NULL and count to 0
 	initAirline(&company);
 
-	for (int i = 0; i < 2; i++)
-	{
-		//This function get the pointer to the struct airline
-		//and adds plane to the array
-		if (!addPlane(&company))
+	int choice;
+
+
+	do {
+		printf("Please choose one of the following options\n");
+		printf("0 - Add Airport\n");
+		printf("1 - Add Plane\n");
+		printf("2 - Add Flight\n");
+		printf("3 - Print Airline\n");
+		printf("4 - Print all Airports\n");
+		printf("5 - Print all flights with plane type\n");
+		printf("- 1 - Quit\n");
+
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
+		case 0:
+			addAirport(&manager);
 			break;
-	}
-	printf("Airline name %s\n", company.name);
-	printPlanesArr(company.planeArr, company.planeCount);
-	freeCompany(&company);
+
+		case 1:
+			addPlane(&company);
+			break;
+
+		case 2:
+			addFlight(&company, &manager);
+			break;
+
+		case 3:
+			printCompany(&company);
+			break;
+
+		case 4:
+			printAirports(&manager);
+			break;
+
+		case 5:
+			doPrintFlightsWithPlaneType(&company);
+			break;
+		}
+	} while (choice != -1);
 }
